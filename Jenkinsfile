a94c3f3d7deaac2713149c6b2c049bc8e6a853ec
TheWebShark <30327147+thewebshark@users.noreply.github.com>
2022-11-21 20:00:06 -0700

    Lab 4 - Updated to use my github repo.

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,12 +2,16 @@
 node {
   stage('checkout sources') {
         // You should change this to be the appropriate thing
-        git url: 'https://github.com/ColumbusStateWorkforceInnovation/special-topics-ci-lab'
+        git url: 'https://github.com/thewebshark/specialtopics-lab4-ci'
   }
 
   stage('Build') {
     // you should build this repo with a maven build step here
     echo "hello"
+    withMaven (maven: 'maven3') {
+      sh "mvn package"
+    }
   }
   // you should add a test report here
+
 }
--- docker/dsl/build_pipeline.groovy
+++ docker/dsl/build_pipeline.groovy
@@ -1,7 +1,7 @@
 pipelineJob('build-pipeline-job') { 
 
   // This is the repo that contains the Jenkinsfile you want to use to define your pipeline
-  def repo = 'https://github.com/ColumbusStateWorkforceInnovation/special-topics-ci-lab'
+  def repo = 'https://github.com/thewebshark/specialtopics-lab4-ci'
 
   description("Your App Pipeline") 
   keepDependencies(false) 
@@ -18,7 +18,7 @@ pipelineJob('build-pipeline-job') {
       scm { 
         git { 
           remote { url(repo) } 
-          branches('master') 
+          branches('main')
           scriptPath('Jenkinsfile') // The location of the Jenkinsfile that defines the pipeline job within the repo
           extensions { }  // required as otherwise it may try to tag the repo, which you may not want 
         } 
